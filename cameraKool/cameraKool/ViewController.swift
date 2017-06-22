5ad74c5614c52052b0eed51faeb138487b22f2fb
Vinh <vinh@Vinhs-Mac.local>
2017-06-22 10:52:35 +0700

    update basic ui and open camera button

--- cameraKool/cameraKool/ViewController.swift
+++ cameraKool/cameraKool/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    // MARK: *** Data model
+    // MARK: *** UI Elements
+    @IBOutlet weak var imagePicked: UIImageView!
+    // MARK: *** UI events
+    // MARK: *** Local variables
+    // MARK: *** UIViewController
+    
+    @IBAction func openCameraButton(_ sender: Any) {
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+                imagePicker.allowsEditing = false
+                self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
