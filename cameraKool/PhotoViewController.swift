40f0fd0d23946bacb0ae48ed238ab291903772f9
Vinh <vinh@Vinhs-Mac.local>
2017-06-30 16:56:44 +0700

    update with photo filters after taking the photo

--- cameraKool/PhotoViewController.swift
+++ cameraKool/PhotoViewController.swift
@@ -29,9 +29,13 @@ class PhotoViewController: UIViewController {
     
     private var backgroundImage: UIImage
     
+    
+    
     init(image: UIImage) {
         self.backgroundImage = image
+        
         super.init(nibName: nil, bundle: nil)
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -42,8 +46,10 @@ class PhotoViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.gray
         let backgroundImageView = UIImageView(frame: view.frame)
+        let BackgroundImageViewTag = 1
         backgroundImageView.contentMode = UIViewContentMode.scaleAspectFit
         backgroundImageView.image = backgroundImage
+        backgroundImageView.tag = BackgroundImageViewTag
         view.addSubview(backgroundImageView)
         let cancelButton = UIButton(frame: CGRect(x: 10.0, y: 10.0, width: 30.0, height: 30.0))
         cancelButton.setImage(#imageLiteral(resourceName: "cancel"), for: UIControlState())
@@ -54,6 +60,14 @@ class PhotoViewController: UIViewController {
         saveButton.addTarget(self, action: #selector(save), for: .touchUpInside)
         view.addSubview(cancelButton)
         view.addSubview(saveButton)
+        
+        
+        let filterScrollView = UIScrollView(frame: CGRect(x: 0, y: (self.view.frame.height)-80, width: (self.view.frame.width), height: 80 ))
+        let FILTER_SCROLLVIEW_TAG  = 2
+        filterScrollView.isUserInteractionEnabled = true
+        filterScrollView.tag = FILTER_SCROLLVIEW_TAG
+        self.createFilterButton(scrollView: filterScrollView, imageView: (self.view))
+        self.view.addSubview(filterScrollView)
     }
     
     func cancel() {
@@ -64,4 +78,83 @@ class PhotoViewController: UIViewController {
         customPhotoAlbum.save(image: self.backgroundImage)
         dismiss(animated: true, completion: nil)
     }
+    
+    func getBackgroundImage() -> UIImage{
+        return backgroundImage
+    }
+    func setBackgroundImage(image: UIImage){
+        backgroundImage = image
+    }
+    func getView() ->UIView{
+        return self.view
+    }
+    
+    func getPhotoViewController() -> PhotoViewController{
+        return self
+    }
+    
+    func createFilterButton(scrollView: UIScrollView, imageView: UIView){
+        var CIFilterNames = [
+            "CIPhotoEffectChrome",
+            "CIPhotoEffectFade",
+            "CIPhotoEffectInstant",
+            "CIPhotoEffectNoir",
+            "CIPhotoEffectProcess",
+            "CIPhotoEffectTonal",
+            "CIPhotoEffectTransfer",
+            "CISepiaTone"
+        ]
+        
+        var xCoord: CGFloat = 5
+        let yCoord: CGFloat = 5
+        
+        let buttonHeight: CGFloat = 70
+        let buttonWidth:CGFloat = buttonHeight*(imageView.frame.width)/(imageView.frame.height)
+        let gapBetweenButtons: CGFloat = 5
+        
+        var itemCount = 0
+        
+        for filterNameNumber in 0..<CIFilterNames.count {
+            itemCount = filterNameNumber
+            
+            // Button properties
+            let filterButton = UIButton(type: .custom)
+            filterButton.frame = CGRect(x: xCoord, y: yCoord, width: buttonWidth, height: buttonHeight)
+            filterButton.tag = itemCount
+            filterButton.addTarget(self, action: #selector(self.filterButtonTapped(sender:)), for: .touchUpInside)
+            filterButton.layer.cornerRadius = 6
+            filterButton.clipsToBounds = true
+            
+            
+            let ciContext = CIContext(options: nil)
+            let coreImage = CIImage(image: (self.backgroundImage))
+            let filter = CIFilter(name: "\(CIFilterNames[filterNameNumber])")
+            filter!.setDefaults()
+            filter!.setValue(coreImage, forKey: kCIInputImageKey)
+            let filteredImageData = filter!.value(forKey: kCIOutputImageKey) as! CIImage
+            let filteredImageRef = ciContext.createCGImage(filteredImageData, from: filteredImageData.extent)
+            
+            let imageForButton = UIImage(cgImage: filteredImageRef!, scale: self.backgroundImage.scale, orientation: backgroundImage.imageOrientation)
+            
+            filterButton.setBackgroundImage(imageForButton, for: .normal)
+            
+            xCoord +=  buttonWidth + gapBetweenButtons
+            scrollView.addSubview(filterButton)
+        }
+        
+        scrollView.contentSize = CGSize(width: buttonWidth * CGFloat(itemCount+2), height: yCoord)
+    }
+    
+    func filterButtonTapped(sender: UIButton) {
+        let button = sender as UIButton
+        
+        let backgroundImage = button.backgroundImage(for: UIControlState.normal)!
+        let BACKGROUND_IMAGEVIEW_TAG = 1
+        let backgroundImageView = self.view.viewWithTag(BACKGROUND_IMAGEVIEW_TAG) as! UIImageView
+        backgroundImageView.image = backgroundImage
+        self.backgroundImage = backgroundImage
+        
+        
+    }
+    
 }
